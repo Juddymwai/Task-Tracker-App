b8b64996f18e73418109484e6b7565a4c6e172f4
Judy Mwai <juddiemukami@gmail.com>
2022-10-10 12:44:23 +0300

    set password and user name to empty sstring inside login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,13 @@
+
+
+function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
