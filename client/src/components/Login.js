ff310b99ac0c42f918a9f67dd3c33a6f23542534
Judy Mwai <juddiemukami@gmail.com>
2022-10-10 12:45:33 +0300

    creates handle submit function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,26 @@
-
+import React, {useState} from "react";
 
 function Login(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch('/login',{
+        method: "POST", 
+        headers: {"Content-Type":"application/json"
+
+        }, 
+
+        body: JSON.stringify({username, password}),
+
+        }).then((r)=>{
+            if (r.ok){ 
+
+                r.json().then((user) => setUser(user));
+
+            }
+        });
+    }
 
 
     return (
