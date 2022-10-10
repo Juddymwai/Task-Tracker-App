72b215299d7ef1062003cec22157374440709731
Judy Mwai <juddiemukami@gmail.com>
2022-10-10 13:03:00 +0300

    adds signup form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -31,6 +31,43 @@ function SignUp(){
 
     return (
         <div>
+              <form onSubmit={handleSubmit}>
+
+            <h1>SignUp</h1>
+
+
+            <input
+                type="text"
+                value={username}
+                id= "username"
+                autoComplete="off"
+                onChange= {(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <br/>
+
+            <input
+                type="password"
+                value={password}
+                id= "password"
+                autoComplete="current-password"
+                onChange= {(e) => setPassword(e.target.value)}
+                placeholder="Password"
+
+            />
+            <br/>
+            <input
+                type="password"
+                value={confirmPassword}
+                id= "confirmPassword"
+                onChange= {(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirm Password"
+                autoComplete="current-password"
+                
+            />
+            <br/>
+            <button>Sign Up</button>
+            </form>
 
         </div>
     )
