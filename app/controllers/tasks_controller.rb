5412058047629acb1be950b99b7771037821fdf1
Judy Mwai <juddiemukami@gmail.com>
2022-10-10 23:39:21 +0300

    adds destroy and show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,30 @@
 class TasksController < ApplicationController
+
+
+    def show 
+        task = Task.find_by(id: session[:user_id])
+        
+        if task
+            render json: task
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end
+
+    end
+
+    def update 
+
+
+    end
+    def destroy 
+
+        task = Task.find_by(id: session[:user_id]) 
+        if task
+            task.destroy(task_params)
+            head :no_content
+        else 
+            render json: {error: "task not found"}, status: :not_found
+
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_083735) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_202821) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "duration"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
