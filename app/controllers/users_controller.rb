6aff049c85690ebfccb2c34aac8a04400c303719
Judy Mwai <juddiemukami@gmail.com>
2022-10-10 11:55:06 +0300

    creates post action inside user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
-    
+    def create 
+        user = User.create (user_params)
+        if user.valid?
+            session[:user_id] = user.id 
+            render json: user, status: :created 
+        else
+            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+    end
 
 end
