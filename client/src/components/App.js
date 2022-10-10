92e86450f2922bc23079b9bd8b6be921eb914604
Judy Mwai <juddiemukami@gmail.com>
2022-10-10 12:42:22 +0300

    adds useeffect inside app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,15 @@
 import React,{ useState,useEffect} from "react";
 
 function App(){
+  const [user, setUser] = useState(null);
+  useEffect(() =>{
+    fetch('/me').then((resp) =>{
+      if (resp.ok){
+        resp.json().then ((user)=> setUser(user));
+      }
+    })
+  }, []
+  );
 
   return (
     <div>
