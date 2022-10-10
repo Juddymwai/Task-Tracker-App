d1790ab5857697bd96084d04c85317f0bdc1c220
Judy Mwai <juddiemukami@gmail.com>
2022-10-10 11:58:25 +0300

    adds a show action inside user contraoller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
 
+
+    def show 
+        user = User.find_by(id: session[:user_id])
+        
+        if user
+            render json: user
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
+        end
+
+    end
+
     def create 
         user = User.create (user_params)
         if user.valid?
