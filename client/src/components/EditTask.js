bb6bbf42908e81a548bfda5810a3f419e4a2afea
Judy Mwai <juddiemukami@gmail.com>
2022-10-12 01:53:36 +0300

    adds handle submit function insise edit component

--- client/src/components/EditTask.js
+++ client/src/components/EditTask.js
@@ -1,19 +1,35 @@
 import React, {useState} from "react"
 
-function EditTask(){
+function EditTask({body, id, onUpdatedTask}){
 
-    const [taskBody, setTaskBody]= useState[""]
+    const [taskBody, setTaskBody]= useState(body);
 
+    function handleSubmit(e){
+        e.preventDefault()
+     
+
+       fetch(`/users/${id}`, {
+           method: "PATCH",
+           headers: {"Content-Type":"application/json"},
+           body: JSON.stringify({
+            body: taskBody
+           })
+
+       })
+       .then((resp) => resp.json())
+       .then((updatedTask) => onUpdatedTask(updatedTask) )
+
+   }
     return (
         <div>
             <form>
                 <input
-                type={text}
-                name ={body}
+                type="text"
+                name = "body"
                 value ={taskBody}
                 onChange ={(e) => setTaskBody(e.target.value)}
-            
                 />
+                <input type="submit" value ="save"/>
             </form>
 
 
