3f8d16ef26cda390d10c152cdb7f6926372fc4c2
Judy Mwai <juddiemukami@gmail.com>
2022-10-11 13:12:52 +0300

    adds fetch inside home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,38 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 function Home({user}){
+
+    const [task, setTask]=useState([]);
+
+    useEffect(()=>{
+        fetch('/tasks')
+     
+        .then((r)=> r.json())
+        .then((data)=> setTask(data))
+        }, [])
+
     if (user){
         return (
             <div>
                 <h2> Welcome, {user.username}!</h2>;
+                <h1>My Tasks</h1>
+                {task.map((tasks) => {
+                    return (
+                        <div>
+                            <ul>
+                                <li>{tasks.title}</li>
+                                <li>{tasks.duration}</li>
+                                <li>{tasks.user_id}</li>
+                            </ul>
+                        </div>
+                    )
+
+
+                })}
+                {/* <ul>
+                    <li>{title} <span>{duration}</span></li>
+
+                </ul> */}
             </div>
         )
 
