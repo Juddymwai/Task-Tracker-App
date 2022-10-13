b2ff9862f16f7890d0e8d799e8b7beb1f03c7e9d
Judy Mwai <juddiemukami@gmail.com>
2022-10-13 22:27:03 +0300

    adds new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :tasks, dependent: :destroy
 
-    # validates :title, presence: true
+    
     validates :username, uniqueness: true
 end
